077a2aae0ae3af60c37a4ffe6e5927884ef881f6
Egan Johnson <egancj2@illinois.edu>
2025-11-18 00:44:14 -0600

    Merge commit

--- src/kernels/linkedlist.cuh
+++ src/kernels/linkedlist.cuh
@@ -42,7 +42,7 @@ struct BlockedList
 
 BlockedList newBlockedList(BlockedListBuffer *buffer);
 
-__device__ ListNode *getNode(BlockedList *bl, int index);
+__host__ __device__ ListNode *getNode(BlockedList *bl, int index);
 
 __device__ unsigned getIndex(BlockedList *bl, ListNode *ln);
 
@@ -55,5 +55,76 @@ __device__ void addToList(BlockedList *bl, ListNode *ln);
 __device__ BlockedList *concatLists(BlockedList *a, BlockedList *b);
 
 __device__ ListNode *newNode(BlockedListBuffer *buffer, unsigned block_size);
+
+/**
+ * @brief Iterator which can be used to iterate through the elements of a
+ * BlockedList. Requires all blocks of the blocked list to be sized a multiple
+ * of the size of datatype given.
+ *
+ * @tparam T Type of value to extract
+ */
+template <typename T>
+struct ListIterator
+{
+    BlockedList *list;
+    ListNode *node;
+    int element_index;
+    bool finished = false;
+
+    /**
+     * @brief Construct a new ListIterator object starting at the beginning of the
+     * linked list given
+     *
+     * @param bl The linked list given
+     */
+    ListIterator(BlockedList *bl)
+    {
+        list = bl;
+        if (bl->start_pointer == NULL_ID)
+            finished = true;
+        else
+        {
+            node = getNode(bl, bl->start_pointer);
+            element_index = 0;
+        }
+    }
+
+    /**
+     * @brief Gets the next element in the list, if there are any left.
+     *
+     * @param result Where the next element will be stored.
+     * @return true If an element was returned
+     * @return false If we're at the end of the list. In this case, "result" is unchanged.
+     */
+    bool next(T *result)
+    {
+        if (finished)
+            return false;
+
+        *result = *((T *)&node->data[element_index]);
+        element_index += sizeof(T);
+        if (element_index >= node->block_size)
+        {
+            element_index = 0;
+            if (node->next_node == NULL_ID)
+                finished = true;
+            else
+                node = getNode(list, node->next_node);
+        }
+
+        return true;
+    }
+
+    /**
+     * @brief Returns if there are any elements left in the list being iterated through.
+     *
+     * @return true
+     * @return false
+     */
+    bool hasNext()
+    {
+        return !finished;
+    }
+};
 
 #endif
